992baabe7112a7f8257c0eb733cf7724f90f420a
Eran Schoellhorn <eschoellhorn@352inc.com>
2016-10-14 20:59:54 -0400

    Re-organize files for smart/presentational project layout

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import App from '../components/App';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+AppContainer.propTypes = {};
+AppContainer.defaultProps = {};
+
+export default AppContainer;
